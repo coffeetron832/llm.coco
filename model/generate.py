a4e76dc7c3672307f2fba3e9a9c7e90107722546
coffeetron832 <jpontonh@gmail.com>
2026-06-30 18:42:36 -0500

    Create generate.py

---
+++ model/generate.py
@@ -0,0 +1,52 @@
+import torch
+
+from tokenizer import CharacterTokenizer
+from transformer import CocoTransformer
+
+
+CHECKPOINT = "../checkpoints/coco_v0.pt"
+
+checkpoint = torch.load(
+    CHECKPOINT,
+    map_location="cpu"
+)
+
+chars = checkpoint["vocab"]
+
+tokenizer = CharacterTokenizer(
+    "".join(chars)
+)
+
+model = CocoTransformer(
+    vocab_size=tokenizer.vocab_size
+)
+
+model.load_state_dict(
+    checkpoint["model_state"]
+)
+
+model.eval()
+
+
+while True:
+
+    prompt = input("\nTú: ")
+
+    if prompt.lower() in ["salir", "exit", "quit"]:
+        break
+
+    context = torch.tensor(
+        [tokenizer.encode(prompt)],
+        dtype=torch.long
+    )
+
+    output = model.generate(
+        context,
+        max_new_tokens=100
+    )
+
+    text = tokenizer.decode(
+        output[0].tolist()
+    )
+
+    print(f"\nCoco: {text}")
